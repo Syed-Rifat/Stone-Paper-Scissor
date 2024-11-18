f44fdd689ff4dbc2efbc570e22851c8b01335fdd
syed_rifat <syedrifat411@gmail.com>
2024-11-19 00:31:00 +0600

    Initial Commit

---
+++ app/src/main/java/com/sdrt/stonepaperscissors/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.sdrt.stonepaperscissors;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 3000; // 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Delay and launch MainActivity
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_TIME_OUT);
+    }
+}
